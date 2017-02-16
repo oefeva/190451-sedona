ae1137dc7ef2ec72870241ee85f1e9707f6f6a65
oefeva <oefeva@gmail.com>
2017-02-16 15:06:32 +0300

    Правки

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 var openCloseForm = document.querySelector(".find-hotel");
 var popUp = document.querySelector(".hotel-search");
-popUp.classList.add("hotel-search");
+popUp.classList.add("hotel-search-hide");
 
-openCloseForm.addEventListener("click", function (event) {
+openCloseForm.addEventListener("click", function(event) {
   event.preventDefault();
   popUp.classList.toggle("hotel-search-hide");
 });
 
 window.addEventListener("keydown", function(event) {
-   if (event.keyCode === 27) {
-     if (popUp.classList.contains("hotel-search")) {
-       popUp.classList.remove("hotel-search");
-     }
-   }
- });
+  if (event.keyCode === 27) {
+    if (!popUp.classList.contains("hotel-search-hide")) {
+      popUp.classList.add("hotel-search-hide");
+    }
+  }
+});
